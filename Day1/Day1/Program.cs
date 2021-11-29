501d2d4de16042f593a6797cb486f331abfdf5a1
Kevin Day <kday@up.com>
2021-11-29 15:23:21 -0600

    Refactor a bit, then adjust for part 2

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,28 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day1
 {
     class Program
     {
-        static void Main(string[] args)
+        static Tuple<int, int> findSumInRange(List<int> numbers, int target_sum, int start_index, int stop_index)
         {
-            using(TextReader reader = File.OpenText("input.txt"))
+            Tuple<int, int> answer = null;
+            bool[] numbers_seen = new bool[target_sum + 1];
+            for(int i=start_index; i < stop_index; ++i)
             {
-                bool[] numbers = new bool[2021];
+                int number = numbers[i];
+                int match = target_sum - number;
+                if(match >= 0)
+                {
+                    if (numbers_seen[match])
+                    {
+                        answer = new Tuple<int, int>(number, match);
+                        break;
+                    }
+                    else
+                    {
+                        numbers_seen[number] = true;
+                    }
+                }
+            }
+            return answer;
+        }
+
+        static List<int> readNumbers(string file_name)
+        {
+            List<int> numbers = new List<int>();
+            using (TextReader reader = File.OpenText(file_name))
+            {
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     int number = int.Parse(line);
-                    int match = 2020 - number;
-                    if (numbers[match])
-                    {
-                        Console.WriteLine("Answer: {0} * {1} = {2}", number, match, number * match);
-                    }
-                    else
-                    {
-                        numbers[number] = true;
-                    }
+                    numbers.Add(number);
+                }
+            }
+            return numbers;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> numbers = readNumbers("input.txt");
+            for (int i = 0; i < numbers.Count; ++i)
+            {
+                int target = 2020 - numbers[i];
+                Tuple<int, int> pair = findSumInRange(numbers, target, 0, i);
+                if (pair != null)
+                {
+                    Console.WriteLine("Answer: {0} * {1} * {2} = {3}", pair.Item1, pair.Item2, numbers[i], pair.Item1 * pair.Item2 * numbers[i]);
+                    break;
                 }
             }
         }
