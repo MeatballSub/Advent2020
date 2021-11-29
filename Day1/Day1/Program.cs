0079681dcfea92e65ff10962248b9aa9ab053ee8
Kevin Day <kday@up.com>
2021-11-29 13:59:13 -0600

    Put input in array

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -9,11 +9,19 @@ namespace Day1
         {
             using(TextReader reader = File.OpenText("input.txt"))
             {
+                bool[] numbers = new bool[2021];
                 string line;
                 while ((line = reader.ReadLine()) != null)
                 {
                     int number = int.Parse(reader.ReadLine());
-                    Console.WriteLine("Read: " + number);
+                    numbers[number] = true;
+                }
+                for(int i = 0; i < 2021; ++i)
+                {
+                    if(numbers[i])
+                    {
+                        Console.WriteLine("Found: " + i);
+                    }
                 }
             }
         }
