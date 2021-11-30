22e92ab06c00d6c5a770811e505f5b084840d733
Kevin Day <kday@up.com>
2021-11-30 01:16:20 -0600

    Add Day 10.2 solution

--- Day10/Day10/Program.cs
+++ Day10/Day10/Program.cs
@@ -19,20 +19,71 @@ namespace Day10
             }
             return joltages;
         }
+
+        public static Dictionary<int, Int64> recurrence = new Dictionary<int, Int64>() { { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 1 } };
+
+        static Int64 getRecurrence(int index)
+        {
+            // a_n = (2 * a_(n-1)) + (2^(n-4)) - a_(n-4)
+            if (!recurrence.ContainsKey(index))
+            {
+                Int64 value = (2 * getRecurrence(index - 1)) + (1 << (index - 4)) - getRecurrence(index - 4);
+                recurrence.Add(index, value);
+            }
+
+            return recurrence[index];
+        }
+
+        static Int64 getMultiplier(int index)
+        {
+            // multiplier = 2^n - a_n
+            return ((1 << index) - getRecurrence(index));
+        }
+
+        static Int64 getArrangements(List<int> differences)
+        {
+            Int64 arrangements = 1;
+            int consecutive = 0;
+            for(int i = 0; i < differences.Count; ++i)
+            {
+                if(differences[i] == 1)
+                {
+                    ++consecutive;
+                }
+                else
+                {
+                    --consecutive;
+                    if (consecutive > 0)
+                    {
+                        Int64 multiplier = getMultiplier(consecutive);
+                        //Console.WriteLine("Consecutive: " + consecutive + ", Multiplier: " + multiplier);
+                        arrangements *= multiplier;
+                    }
+                    consecutive = 0;
+                }
+            }
+
+            return arrangements;
+        }
+
         static void Main(string[] args)
         {
             int [] counts = new int[4] { 0,0,0,0 };
             List<int> joltages = readInput("input.txt");
+            List<int> differences = new List<int>();
             joltages.Add(0);
             joltages.Sort();
             for(int i = 1; i < joltages.Count; ++i)
             {
                 int difference = (joltages[i] - joltages[i - 1]);
+                differences.Add(difference);
                 ++counts[difference];
             }
             ++counts[3];
+            differences.Add(3);
             Console.WriteLine("Differences: 0({0}) 1({1}) 2({2}) 3({3})", counts[0], counts[1], counts[2], counts[3]);
             Console.WriteLine("Answer: {0} * {1} = {2}", counts[1], counts[3], counts[1] * counts[3]);
+            Console.WriteLine("Arrangements: " + getArrangements(differences));
         }
     }
 }
