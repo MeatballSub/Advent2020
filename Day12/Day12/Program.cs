fbcba6bb99adca932ac8ef30d74240c08096c732
Kevin Day <kday@up.com>
2021-11-30 19:26:06 -0600

    Add Day 12.2 solution

--- Day12/Day12/Program.cs
+++ Day12/Day12/Program.cs
@@ -38,14 +38,21 @@ namespace Day12
             return instructions;
         }
 
-        static Point execute(List<Instruction> instructions)
+        static Point rotate(Point point, int left_rotate_index)
+        {
+            List<List<int>> rotations = new List<List<int>>() { new List<int>(){1,0,0,1}, new List<int>() { 0, -1, 1, 0 }, new List<int>() { -1, 0, 0, -1 }, new List<int>() { 0, 1, -1, 0 } };
+            List<int> rotation = rotations[left_rotate_index];
+            return new Point(rotation[0] * point.X + rotation[1] * point.Y, rotation[2] * point.X + rotation[3] * point.Y);
+        }
+
+        static Point execute1(List<Instruction> instructions)
         {
             List<Point> directions = new List<Point>() { new Point(1, 0), new Point(0, 1), new Point(-1, 0), new Point(0, -1) };
             Point offset = new Point(0, 0);
             int facing = 0;
-            foreach(Instruction instruction in instructions)
+            foreach (Instruction instruction in instructions)
             {
-                switch(instruction.type)
+                switch (instruction.type)
                 {
                     case 'N':
                         offset.Offset(0, instruction.operand);
@@ -54,7 +61,7 @@ namespace Day12
                         offset.Offset(0, -instruction.operand);
                         break;
                     case 'E':
-                        offset.Offset(instruction.operand,0);
+                        offset.Offset(instruction.operand, 0);
                         break;
                     case 'W':
                         offset.Offset(-instruction.operand, 0);
@@ -64,13 +71,55 @@ namespace Day12
                         break;
                     case 'R':
                         facing = (facing - (instruction.operand / 90)) % directions.Count;
-                        if(facing < 0)
+                        if (facing < 0)
                         {
                             facing += directions.Count;
                         }
                         break;
                     case 'F':
                         offset.Offset(directions[facing].X * instruction.operand, directions[facing].Y * instruction.operand);
+                        break;
+                }
+            }
+
+            return offset;
+        }
+
+        static Point execute2(List<Instruction> instructions)
+        {
+            Point offset = new Point(0, 0);
+            Point facing = new Point(10, 1);
+            int left_rotate_index = 0;
+            foreach (Instruction instruction in instructions)
+            {
+                switch (instruction.type)
+                {
+                    case 'N':
+                        facing.Offset(0, instruction.operand);
+                        break;
+                    case 'S':
+                        facing.Offset(0, -instruction.operand);
+                        break;
+                    case 'E':
+                        facing.Offset(instruction.operand, 0);
+                        break;
+                    case 'W':
+                        facing.Offset(-instruction.operand, 0);
+                        break;
+                    case 'L':
+                        left_rotate_index = (instruction.operand / 90) % 4;
+                        facing = rotate(facing, left_rotate_index);
+                        break;
+                    case 'R':
+                        left_rotate_index = (-instruction.operand / 90) % 4;
+                        if (left_rotate_index < 0)
+                        {
+                            left_rotate_index += 4;
+                        }
+                        facing = rotate(facing, left_rotate_index);
+                        break;
+                    case 'F':
+                        offset.Offset(facing.X * instruction.operand, facing.Y * instruction.operand);
                         break;
                 }
             }
@@ -85,12 +134,18 @@ namespace Day12
 
         static void Main(string[] args)
         {
+            //Point p = new Point(1, 0);
+            //Console.WriteLine(rotate(p, 1));
+            //Console.WriteLine(rotate(p, 2));
+            //Console.WriteLine(rotate(p, 3));
+            //Console.WriteLine(rotate(p, 0));
+
             List<Instruction> instructions = readInput("input.txt");
             //foreach(Instruction instruction in instructions)
             //{
             //    Console.WriteLine(instruction.ToString());
             //}
-            Point offset = execute(instructions);
+            Point offset = execute2(instructions);
             Console.WriteLine(offset.ToString());
             int manhattan_distance = getManhattanDistance(offset);
             Console.WriteLine("Manhattan Distance: " + manhattan_distance);
